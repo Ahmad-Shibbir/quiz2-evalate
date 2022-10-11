36a9e7c3417f27c5cb0efe1e4031817cf86d55b7
Ahmad-Shibbir <ahmadshibbir.iium@gmail.com>
2022-10-12 02:19:06 +0800

    line chart added

--- src/Components/Statistics/Statistics.js
+++ src/Components/Statistics/Statistics.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+
+import { useLoaderData } from 'react-router-dom';
 
 const Statistics = () => {
+    const quizInfo = useLoaderData();
+    
     return (
         <div>
-           <h1>This is Statistics Page</h1> 
+            <div><h1 className='font-bold text-4xl text-center m-8'>Number of Quizzes for different topic</h1></div>
+            <div className='flex justify-center m-20'>
+            <LineChart width={500} height={400} data={quizInfo.data}>
+                    <Line type='monotone' dataKey='total' stroke='#82cacd'></Line>
+                    <XAxis dataKey='name'></XAxis>
+                    <YAxis></YAxis>
+                    <Tooltip></Tooltip>
+            </LineChart>
+            </div>
         </div>
+       
     );
 };
 
