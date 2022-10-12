34293871725e26429ff56ee2314cb6f16472845c
Ahmad-Shibbir <ahmadshibbir.iium@gmail.com>
2022-10-12 22:12:20 +0800

    toaster added for display correct answer when user clicked on eye icon

--- src/Components/Quiz/Quiz.js
+++ src/Components/Quiz/Quiz.js
@@ -9,21 +9,11 @@ const Quiz = () => {
     const {questions}=quiz.data
     console.log(quiz)
 
-    let eyeClick;
-    let eye = false;
+    
 
 
-    const displayAnswer= ()=>{
-        eye = !eye;
-
-        if(eye){
-            eyeClick = <p>Answer</p>
-            console.log('true clicked')
-        }
-        else{
-            eyeClick = <p> </p>
-            console.log('falll clicked')
-        }
+    const displayAnswer= (ans)=>{
+        toast(ans);
     }
     const answerCheck= (question, option)=>{
         console.log(option)
@@ -48,7 +38,10 @@ const Quiz = () => {
                     <div className="card  bg-black shadow-xl my-10 p-8 mx-40">
                         <h1 className='text-3xl font-bold'> {question.question.replace(/<\/?[^>]+>/gi, '')}
                         
-                        <svg onClick={()=>displayAnswer()} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                        <svg onClick={()=>displayAnswer(question.correctAnswer)} 
+                        
+                        
+                        xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z" />
                         <path strokeLinecap="round" strokeLinejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
                         </svg>
@@ -58,10 +51,6 @@ const Quiz = () => {
 
 
                         <div className=' '>
-                       
-                            
-                            
-                            <h3>{eye}</h3>
                             <h3 className='font-bold'>{question.options.map(option=>
                             
                             <li>
